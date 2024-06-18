59783f5eda98206bf63d9488829756d0ae49e1a3
Abe <abraham120604@gmail.com>
2024-06-19 05:39:40 +0700

    just changed main to app

--- react/src/components/Main.tsx
+++ react/src/components/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Main = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1>Main</h1>
+      <h1>App</h1>
     </div>
   );
 };
